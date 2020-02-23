8071a32a0886a50025ae977b23aafe39ee1a84c4
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-02-23 09:34:13 -0500

    Some navigation style changes

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -4,54 +4,60 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 
-const cssClasses = 'flex-1 text-center py-4 text-sm text-themeGray-300 hover:text-white';
+const cssClasses = 'text-center py-4 px-4 text-sm text-themeGray-300 hover:text-white';
 
 const Header = ({ isLoggedIn, itemsInCart, isAdmin }) => (
   <header className="fixed top-0 w-full bg-themeGray-700 z-10">
-    <nav className="NavigationBar inner-wrap flex justify-around text-white">
-      <Link href="/">
-        <a className={cssClasses}>Home</a>
-      </Link>
-      <Link href="/shop">
-        <a className={cssClasses}>Shop</a>
-      </Link>
-      <Link href="/category/[categoryName]" as="/category/mobile">
-        <a className={cssClasses}>#mobile</a>
-      </Link>
-      {isAdmin && (
-        <>
-          <Link href="/admin/users">
-            <a className={cssClasses}>Users</a>
-          </Link>
-          <Link href="/admin/products">
-            <a className={cssClasses}>Products</a>
-          </Link>
-        </>
-      )}
-      {isLoggedIn ? (
-        <>
-          <Link href="/orders">
-            <a className={cssClasses}>Orders</a>
-          </Link>
-          <Link href="/logout">
-            <a className={cssClasses}>Logout</a>
-          </Link>
-        </>
-      ) : (
-        <Link href="/login">
-          <a className={cssClasses}>Login</a>
+    <div className="inner-wrap flex flex-wrap items-center justify-center ">
+      <nav className="NavigationBar flex justify-center text-white">
+        <Link href="/">
+          <a className={cssClasses}>Home</a>
         </Link>
-      )}
-      {itemsInCart >= 0 ? (
-        <Link href="/cart">
-          <a className={cssClasses}>
-            My Cart
-            {' '}
-            {itemsInCart > 0 ? `(${itemsInCart})` : ''}
-          </a>
+        <Link href="/shop">
+          <a className={cssClasses}>Shop</a>
         </Link>
-      ) : null}
-    </nav>
+        <Link href="/category/[categoryName]" as="/category/mobile">
+          <a className={cssClasses}>#mobile</a>
+        </Link>
+        {isAdmin && (
+          <>
+            <Link href="/admin/users">
+              <a className={cssClasses}>Users</a>
+            </Link>
+            <Link href="/admin/products">
+              <a className={cssClasses}>Products</a>
+            </Link>
+          </>
+        )}
+        {isLoggedIn ? (
+          <>
+            <Link href="/orders">
+              <a className={cssClasses}>Orders</a>
+            </Link>
+            <Link href="/logout">
+              <a className={cssClasses}>Logout</a>
+            </Link>
+          </>
+        ) : (
+          <Link href="/login">
+            <a className={cssClasses}>Login</a>
+          </Link>
+        )}
+        {itemsInCart >= 0 ? (
+          <Link href="/cart">
+            <a className={cssClasses}>
+              My Cart
+              {' '}
+              {itemsInCart > 0 ? `(${itemsInCart})` : ''}
+            </a>
+          </Link>
+        ) : null}
+      </nav>
+      <div className="relative flex items-center">
+        <input className="rounded-full px-3" type="search" placeholder="Not yet implemented!" />
+        <button className="text-white px-2 py-2 mx-1 absolute right-0" type="submit"><img src="/search-icon.svg" alt="Search Icon" /></button>
+      </div>
+    </div>
   </header>
 );
 
