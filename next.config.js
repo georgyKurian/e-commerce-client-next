7ac2e00d1ae9e35b6c62ec24400596922ce49b1e
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-07-16 22:40:11 -0400

    Removed and rearranged some packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-const withCSS = require('@zeit/next-css');
-const withPurgeCss = require('next-purgecss');
-
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
   poweredByHeader: false,
   distDir: 'dist',
-}
+};
