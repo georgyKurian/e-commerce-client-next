e7b6aa5c047f7e1910b6b3cf5522ef96ab87d027
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-07-22 11:51:29 -0400

    Fixed mobile header

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -5,6 +5,7 @@ import { connect, useSelector } from 'react-redux';
 
 import HamburgerIcon from '../../images/icons/hamburger-icon.svg';
 import ShoppingBag from '../../images/icons/shopping-bag.svg';
+import SearchIcon from '../../images/icons/search-icon.svg';
 
 const linkDataList = [
   'help',
@@ -23,109 +24,114 @@ const Header = ({ children, isFixed, itemsInCart }) => {
   }
 
   return (
-    <header className="relative w-full">
-      <div className={`${isFixed ? 'fixed top-0 z-10' : ''} w-full px-5 xl:px-10 border-b border-gray-400 outer-wrap bg-white`}>
-        <div className="justify-end hidden w-full lg:flex">
-          <ul className="flex text-xs">
-            {linkDataList.map((linkData) => (
-              <li key={linkData}>
-                <Link href="/">
-                  <a className="flex items-center h-8 px-3 text-gray-800 lowercase">{linkData}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <button className="w-8 h-8 text-gray-400 lg:hidden" type="button" aria-label="Open mobile menu">
-          <HamburgerIcon className="fill-current" />
-        </button>
-
-        <div className="flex flex-wrap items-center justify-between">
-          <div>
-            <div className="absolute inset-y-0 flex items-center">
-              <span className="text-5xl">LOGO</span>
-              <span className="sr-only">Company Name</span>
-            </div>
+    <header>
+      <div className={`${isFixed ? 'fixed top-0 z-10' : ''} w-full xl:px-10 border-b border-gray-400 outer-wrap bg-white`}>
+        <div className="relative">
+          <div className="justify-end hidden w-full lg:flex">
+            <ul className="flex text-xs">
+              {linkDataList.map((linkData) => (
+                <li key={linkData}>
+                  <Link href="/">
+                    <a className="flex items-center h-8 px-3 text-gray-800 lowercase">{linkData}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
 
-          <div className="flex">
-            <nav className="top-menu" aria-label="Shop Navigation">
-              <div>
-                <Link href="/">
-                  <a className="">Home</a>
-                </Link>
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex-1">
+              <button className="overflow-hidden text-gray-800 lg:hidden" type="button" aria-label="Open mobile menu">
+                <HamburgerIcon className="w-16 h-12 p-3 stroke-current" />
+              </button>
+            </div>
+            <div>
+              <div className="inset-0 flex items-center lg:absolute">
+                <span className="text-lg lg:text-5xl">LOGO</span>
+                <span className="sr-only">Company Name</span>
               </div>
-              <div>
-                <Link href="/category/[categoryName]" as="/category/shoes">
-                  <a>#Shoes</a>
-                </Link>
-              </div>
-              {user?.isAdmin && (
-              <>
+            </div>
+
+            <div className="hidden lg:flex">
+              <nav className="top-menu" aria-label="Shop Navigation">
                 <div>
-                  <Link href="/admin/users">
-                    <a>Users</a>
+                  <Link href="/">
+                    <a className="">Home</a>
                   </Link>
                 </div>
                 <div>
-                  <Link href="/admin/products">
-                    <a>Products</a>
+                  <Link href="/category/[categoryName]" as="/category/shoes">
+                    <a>#Shoes</a>
                   </Link>
                 </div>
-              </>
-              )}
-              {user?.isLoggedIn ? (
+                {user?.isAdmin && (
                 <>
                   <div>
-                    <Link href="/orders">
-                      <a>Orders</a>
+                    <Link href="/admin/users">
+                      <a>Users</a>
                     </Link>
                   </div>
-
                   <div>
-                    <Link href="/logout">
-                      <a>Logout</a>
+                    <Link href="/admin/products">
+                      <a>Products</a>
                     </Link>
                   </div>
                 </>
-              ) : (
-                <div>
-                  <Link href="/login">
-                    <a>Login</a>
-                  </Link>
-                </div>
-              )}
-            </nav>
-          </div>
-          <div className="flex justify-between">
-            <div className="relative flex items-center my-2">
-              {isButtonToggled ? (
-                <>
-                  <input aria-label="search" className="h-8 pl-3 pr-8 text-gray-700 rounded-full" type="text" placeholder="To be implemented!" />
-                  <button className="absolute right-0 px-2 py-2 mx-1 text-white" type="submit"><img src="/search-icon.svg" alt="Search Icon" /></button>
-                </>
-              ) : (
-                <button type="button" onClick={handleSearchToggleClick}><img src="/search-icon.svg" alt="Search Icon" /></button>
-              )}
+                )}
+                {user?.isLoggedIn ? (
+                  <>
+                    <div>
+                      <Link href="/orders">
+                        <a>Orders</a>
+                      </Link>
+                    </div>
+
+                    <div>
+                      <Link href="/logout">
+                        <a>Logout</a>
+                      </Link>
+                    </div>
+                  </>
+                ) : (
+                  <div>
+                    <Link href="/login">
+                      <a>Login</a>
+                    </Link>
+                  </div>
+                )}
+              </nav>
             </div>
-            {itemsInCart >= 0 ? (
-              <Link href="/cart">
-                <a>
-                  <div className="relative">
-                    <ShoppingBag className="inline w-8 h-8 p-1 text-gray-700 fill-current" />
-                    {' '}
-                    {itemsInCart > 0 ? (
-                      <div className="absolute inset-y-0 right-0">
-                        <div className="flex items-center justify-center w-4 h-4 -mr-1 text-xs text-center text-white bg-red-700 rounded-full leading-5">
-                          {itemsInCart}
+            <div className="flex justify-end flex-1">
+              <div className="relative flex items-center my-2">
+                {isButtonToggled ? (
+                  <>
+                    <input aria-label="search" className="h-8 pl-3 pr-8 text-gray-700 rounded-full" type="text" placeholder="To be implemented!" />
+                    <button className="absolute right-0 px-2 py-2 mx-1 text-white" type="submit"><img src="/search-icon.svg" alt="Search Icon" /></button>
+                  </>
+                ) : (
+                  <button type="button" className="p-2 mr-2" onClick={handleSearchToggleClick}>
+                    <SearchIcon className="w-5 h-5" role="presentation" />
+                  </button>
+                )}
+              </div>
+              {itemsInCart >= 0 ? (
+                <Link href="/cart">
+                  <a className="flex items-center mr-5">
+                    <div className="relative">
+                      <ShoppingBag className="inline w-8 h-8 p-1 text-gray-700 fill-current" />
+                      {' '}
+                      {itemsInCart > 0 ? (
+                        <div className="absolute inset-y-0 right-0">
+                          <div className="flex items-center justify-center w-4 h-4 -mr-1 text-xs text-center text-white bg-red-700 rounded-full leading-5">
+                            {itemsInCart}
+                          </div>
                         </div>
-                      </div>
-                    ) : ''}
-                  </div>
-                </a>
-              </Link>
-            ) : null}
+                      ) : ''}
+                    </div>
+                  </a>
+                </Link>
+              ) : null}
+            </div>
           </div>
         </div>
       </div>
