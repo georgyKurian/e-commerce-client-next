9380521563273137b22385126c2ae9ce135d0cd1
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-07-29 10:58:31 -0400

    CHnaged to function component

--- src/pages/logout.jsx
+++ src/pages/logout.jsx
@@ -1,29 +1,24 @@
 import localStore from 'store2';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Router from 'next/router';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { authLogout } from '../redux/actions/auth';
 import MyLayout from '../components/Layouts/MyLayout';
 
-class Logout extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props;
+const Logout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
     localStore.clear('auth');
     dispatch(authLogout());
     Router.push('/');
-  }
+  });
 
-  render() {
-    return (
-      <MyLayout>
-        Please wait while logging out...
-      </MyLayout>
-    );
-  }
-}
-
-Logout.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  return (
+    <MyLayout>
+      Please wait while logging out...
+    </MyLayout>
+  );
 };
-export default connect()(Logout);
+
+export default Logout;
