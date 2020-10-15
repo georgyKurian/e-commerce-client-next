67a947c60c4c868ae0bb1b28af962bbe76f2621b
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-10-14 21:56:30 -0400

    FIxed browser console warnings

--- src/components/Layouts/MyLayout.jsx
+++ src/components/Layouts/MyLayout.jsx
@@ -4,18 +4,27 @@ import { connect } from 'react-redux';
 import { useRef } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import Meta from './Meta';
 
 const MyLayout = ({
   headerContent, children, title, isPaddingTop, isNavFixed = true,
 }) => {
   const pageWrapperElement = useRef(null);
   const mainCss = (isPaddingTop) ? 'pt-4 md:pt-6 overflow-hidden lg:pt-8 xl:pt-12' : '';
+
   return (
     <>
       <Head>
         <title>{`E | ${title}`}</title>
-        <Meta />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta key="msapplication-TileColor" name="msapplication-TileColor" content="#da532c" />
+        <meta key="theme-color" name="theme-color" content="#ffffff" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       </Head>
       <div ref={pageWrapperElement}>
         <Header isFixed={isNavFixed} pageWrapperElement={pageWrapperElement}>
